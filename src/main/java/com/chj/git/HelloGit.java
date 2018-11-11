2bc4bab914ecc3a23fda0a1babbe2aabacc48940
tom <tom@sina.com>
2018-11-11 10:37:49 +0800

    wsgu

--- src/main/java/com/chj/git/HelloGit.java
+++ src/main/java/com/chj/git/HelloGit.java
@@ -5,7 +5,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		for(int i = 0;i<10;i++) {
 			System.out.println("这是我第一次使用github"); //我是hotfix修改的
-			//我是hotfix合作开发的修改的
+			//我是hotfix合作开发的修改的 对啊
 		}	
 	}
 
